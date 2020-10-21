ddff06c16158ad62ca1695d2d83f4b8c70cdcee3
jinho <gogod23@ajou.ac.kr>
2020-10-21 18:21:59 +0900

    react practice

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class Subject extends Component {
+    render() {
+        return (
+            <header>
+                <h1>WEB</h1>
+                fucn
+            </header>
+        );
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Subject></Subject>
+            </div>
+        );
+    }
+}
+
+export default App;
