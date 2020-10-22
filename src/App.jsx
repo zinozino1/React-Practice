e99a3fd01676efeddca7a0743bdf7a24e7a7f05a
jinho <gogod23@ajou.ac.kr>
2020-10-22 18:10:30 +0900

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
-
-class Subject extends Component {
-    render() {
-        return (
-            <header>
-                <h1>WEB</h1>
-                fucn
-            </header>
-        );
-    }
-}
+import Subject from "./components/subject";
+import Nav from "./components/nav";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Subject></Subject>
+                <Subject title="Sub1" desc="sub1입니다."></Subject>
+                <Subject title="Sub2" desc="sub2입니다."></Subject>
+                <Nav id="fuck"></Nav>
             </div>
         );
     }
---
+++ src/components/nav.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+    render() {
+        return (
+            <ul id={this.props.id}>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        );
+    }
+}
+
+export default Nav;
